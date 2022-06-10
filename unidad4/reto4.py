d1d16654afa15b7286187538d92c7454d7d2d6ae
diegojojoayandun <3922@holbertonschool.com>
2022-06-10 16:13:59 -0500

    Fix PEP8

--- unidad4/reto4.py
+++ unidad4/reto4.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 from functools import reduce
+
+
 def ordenes(rutinaContable):
     """
     bitácora acerca de los registros diarios de una papelería
@@ -9,20 +11,21 @@ def ordenes(rutinaContable):
     dict_ordenes = {}
 
     for orden in rutinaContable:
-        result =tuple(filter( lambda elemento: type(elemento) == tuple, orden))
-        totales = list(map(lambda elemento: elemento[1] * elemento[2], result))
-        sum_totales = reduce(lambda sum_tot, elemento: sum_tot + elemento, totales)
+        result = tuple(filter(lambda element: type(element) == tuple, orden))
+        ls_total = list(map(lambda element: element[1] * element[2], result))
+        totales = reduce(lambda sum_tot, element: sum_tot + element, ls_total)
 
-        if sum_totales < 600000:
-            sum_totales += 25000
-        dict_ordenes[orden[0]]= sum_totales
+        if totales < 600000:
+            totales += 25000
+        dict_ordenes[orden[0]] = totales
 
     print('----------------- Inicio Registro diario --------------------------')
-    for k,v in dict_ordenes.items():
+    for k, v in dict_ordenes.items():
         print('La factura {} tiene un total en pesos de {:,.2f}'.format(k, v))
     print('----------------- Fin Registro diario -----------------------------')
 
 
-
-
-ordenes([[1201, ("5464", 4, 25842.99), ("7854",18,23254.99), ("8521", 9, 48951.95)], [1202, ("8756", 3, 115362.58),("1112",18,2354.99)], [1203, ("2547", 1, 125698.20), ("2635", 2, 135645.20), ("1254", 1, 13645.20),("9965", 5, 1645.20)], [1204, ("9635", 7, 11.99), ("7733",11,18.99), ("88112", 5, 390.95)]])
+ordenes([[1201, ("5464", 4, 25842.99), ("7854", 18, 23254.99), ("8521", 9, 48951.95)],
+         [1202, ("8756", 3, 115362.58), ("1112", 18, 2354.99)],
+         [1203, ("2547", 1, 125698.20), ("2635", 2, 135645.20), ("1254", 1, 13645.20), ("9965", 5, 1645.20)],
+         [1204, ("9635", 7, 11.99), ("7733", 11, 18.99), ("88112", 5, 390.95)]])
