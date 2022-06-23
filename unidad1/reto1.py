97d9029c724e27a24c6b932c379f9f64f3913c94
diegojojoayandun <3922@holbertonschool.com>
2022-06-23 07:51:31 -0500

    foramt code

--- unidad1/reto1.py
+++ unidad1/reto1.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
+
 def CDT(usuario, capital, tiempo):
-    '''Calcula los intereses a un termino de tiempo y capital para
+    """Calcula los intereses a un termino de tiempo y capital para
     un usuario definidos por parametro
     parametros: usuario, capital, tiempo
     salida: string total
-    '''
+    """
 
     total = 0
 
@@ -14,8 +15,10 @@ def CDT(usuario, capital, tiempo):
     else:
         total = int(capital + ((capital * 0.03 * tiempo) / 12))
 
-    return 'Para el usuario {:s} La cantidad de dinero a recibir, según el monto inicial {:d} para un tiempo de {:d} meses es: {:.1f}'.format(usuario, capital, tiempo, total)
+    return "Para el usuario {:s} La cantidad de dinero a recibir, según el monto inicial {:d} para un tiempo de {:d} meses es: {:.1f}".format(
+        usuario, capital, tiempo, total
+    )
 
 
-print(CDT('AB1012', 1000000, 3))
-print(CDT('ER3366', 650000, 2))
+print(CDT("AB1012", 1000000, 3))
+print(CDT("ER3366", 650000, 2))
